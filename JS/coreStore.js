96ce7f5369e21fd2a274868fcbc3f02dcb70cad4
Mahdi Taremi <99602123+Mahdi-Taremi@users.noreply.github.com>
2022-02-17 21:59:05 +0330

    add firs js v1

--- JS/coreStore.js
+++ JS/coreStore.js
@@ -1,1 +1,10 @@
-console.log("Hi");
+const button = document.querySelector('#button');
+const showModal = document.querySelector(".modual")
+
+button.addEventListener("click",event =>{
+   showModal.style.display = "block";
+});
+
+// function myFunction() { 
+//     document.getElementById("#modual").showModal(); 
+//   } 
