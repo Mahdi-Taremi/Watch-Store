5ea825c585940547615ed061b879ac96700a1738
Mahdi Taremi <99602123+Mahdi-Taremi@users.noreply.github.com>
2022-02-21 12:46:47 +0330

    Update js v2

---
+++ JS/products.js
@@ -0,0 +1,74 @@
+export const productsData = [
+    {
+        id: 1,
+        title : "Rolex Men Model : 984003",
+        price: "97$",
+        imageUrl : "./asset/img/Rolex/Rolex-Men1.jpg",
+    },
+    {
+        id: 2,
+        title : "Rolex Men Model : RLX80",
+        price: "119$",
+        imageUrl : "./asset/Rolex/img/Rolex-Men2.jpg",
+    },
+    {
+        id: 3,
+        title : "Rolex Men Model : 5412",
+        price: "99.5$",
+        imageUrl : "./asset/Rolex/img/Rolex-Men3.jpg",
+    },
+    {
+        id: 4,
+        title : "Casio Model : GW-9400-3A",
+        price: "179$",
+        imageUrl : "./asset/Rolex/img/Casio-Men1.jpg",
+    },
+    {
+        id: 5,
+        title : "Casio Model : PRG-240-5DR",
+        price: "112$",
+        imageUrl : "./asset/Rolex/img/Casio-Men2.jpg",
+    },
+    {
+        id: 6,
+        title : "Romanson Model : AL9A11HMNR36R",
+        price: "99.5$",
+        imageUrl : "./asset/Rolex/img/Romanson-Men1.jpg",
+    },
+    {
+        id: 7,
+        title : "Romanson Model : CB5A09HMNJ1R5",
+        price: "135$$",
+        imageUrl : "./asset/Rolex/img/Romanson-Women1.jpg",
+    }, 
+    {
+        id: 8,
+        title : "Royal London Model : RL-41264-06",
+        price: "156$",
+        imageUrl : "./asset/Rolex/img/Royal London-Men1.jpg",
+    },
+    {
+        id: 9,
+        title : "Royal London Model : RL-41264-07",
+        price: "139.5$",
+        imageUrl : "./asset/Rolex/img/Royal London-Men2.jpg",
+    },
+    {
+        id: 10,
+        title : "Seiko Model : SRQ015J1",
+        price: "1310$",
+        imageUrl : "./asset/Rolex/img/Seiko-Men1.jpg",
+    },
+    {
+        id: 11,
+        title : "Seiko Model : SAST003G",
+        price: "1279$",
+        imageUrl : "./asset/Rolex/img/Seiko-Men2.jpg",
+    },
+    {
+        id: 12,
+        title : "Seiko Model : SPB027J1",
+        price: "1265$",
+        imageUrl : "./asset/Rolex/img/Seiko-Men3.jpg",
+    },
+]
